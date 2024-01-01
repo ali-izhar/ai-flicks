45bd0855b53bc95a3664273d066dcdbbea78081d
Izhar Ali <aliizh94@students.rowan.edu>
2023-12-31 19:02:33 -0500

    updated secrets

--- run.py
+++ run.py
@@ -6,8 +6,7 @@ app = create_app()
 if __name__ == "__main__":
     if os.getenv('FLASK_DEBUG') == 'production':
         # Azure deployment
-        port = int(os.environ.get('PORT', 8000))
-        app.run(host='0.0.0.0', port=port)
+        app.run(host='0.0.0.0', port=8000)
     else:
         # Local development
         app.run(debug=True)
